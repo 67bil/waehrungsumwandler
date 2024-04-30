2c621d5ae5459c4fdab56f9c0fab184a57d9c2b5
67bil <166758805+67bil@users.noreply.github.com>
2024-04-30 09:57:09 +0000

    update

--- Währungsumwandler.py
+++ Währungsumwandler.py
@@ -1,8 +1,11 @@
 from abc import get_cache_token
 import tkinter as tk
 
-def update_option(*args):
-    selected_option.set(entry.get())
+def validate_input(input):
+    if input.isdigit():
+        return True
+    else:
+        return False
 
 root = tk.Tk()
 root.title('Währungsumrechner')
@@ -10,41 +13,54 @@ root.geometry('500x500')
 
 options = ['EUR', 'TRY', 'CNY', 'USD', 'COP', 'HUF', 'PHP', 'VB', 'PLN', 'RUB', 'KGS', 'GBP', 'JPY', 'THB', 'ALL', 'KRW']
 
-selected_option = tk.StringVar(root)
-selected_option.set(options[0])  # 
-
-# Label for Währung
 label_wahrung = tk.Label(root, text='Währung', font=('Helvetica', 12))
 label_wahrung.pack(pady=20)
-
 
 frame = tk.Frame(root)
 frame.pack(pady=10)
 
-dropdown1 = tk.OptionMenu(frame, selected_option, *options[:len(options)//2])
+selected_option = tk.StringVar(root)
+selected_option.set(options[0])
+
+dropdown1 = tk.OptionMenu(frame, selected_option, *options)
 dropdown1.pack(side=tk.LEFT)
 
 selected_option2 = tk.StringVar(root)
-selected_option2.set(options[len(options)//2]) 
+selected_option2.set(options[0])
 
-dropdown2 = tk.OptionMenu(frame, selected_option2, *options[len(options)//2:])
+dropdown2 = tk.OptionMenu(frame, selected_option2, *options)
 dropdown2.pack(side=tk.RIGHT)
 
-# Legende/Wechselkurse
-def create_window():
-    window = tk.Toplevel(root)
+def validate_decimal_input(input):
+    if input.replace('.', '', 1).isdigit():
+        return True
+    else:
+        return False
 
-b1 = tk.Button(root, text="Wechselkurse", command=create_window)
-b1.pack(side=tk.RIGHT, anchor=tk.SE, padx=10, pady=10)  
 
-# 
-def show_legende():
-    legend_window = tk.Toplevel(root)
-    legend_window.title("Legende")
-    import legende.py as legende
-    # 
 
-b2 = tk.Button(root, text="Legende", command=show_legende)
-b2.pack(side=tk.RIGHT, anchor=tk.SE, padx=10, pady=10)
+start_wahrung_label = tk.Label(root, text='Startwährung', font=('Helvetica', 12))
+start_wahrung_label.pack(side=tk.LEFT)
+ziel_wahrung_label = tk.Label(root, text='Zielwährung', font=('Helvetica', 12))
+ziel_wahrung_label.pack(side=tk.LEFT)
+
+entry_var = tk.StringVar()
+entry = tk.Entry(root, textvariable=entry_var)
+entry.pack()
+output_var = tk.StringVar()
+output_field = tk.Entry(root, textvariable=output_var, state='readonly')
+output_field.pack()
+
+
+
+
+def reset():
+  entry_var.set('')
+  output_var.set('')
+
+berechnen_button = tk.Button(root, text='Berechnen')
+berechnen_button.pack(side=tk.LEFT, padx=10)
+zurucksetzen_button = tk.Button(root, text='Zurücksetzen', command=reset)
+zurucksetzen_button.pack(side=tk.RIGHT, padx=10)
 
 root.mainloop()
