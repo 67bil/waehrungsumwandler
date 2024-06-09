e62595e4e3f6d5126c22f06f803cba7397f01331
67bil <166758805+67bil@users.noreply.github.com>
2024-06-10 00:50:25 +0200

    Finished Product

--- Währungsumwandler.py
+++ Währungsumwandler.py
@@ -1,157 +1,134 @@
-from abc import get_cache_token
 import tkinter as tk
-import currencyapicom
-client = currencyapicom.Client('cur_live_nQra2J0SGFy6q39HriU9LkK8lRUzXoOe4ELKIENr')
+import currencyapicom #API
+client = currencyapicom.Client('cur_live_nQra2J0SGFy6q39HriU9LkK8lRUzXoOe4ELKIENr') #API Key
 
 root = tk.Tk()
 root.title('Währungsumwandler')
 root.geometry('500x500')
-font= ("Courier")
 
-label_wahrung = tk.Label(root, text='Währungsumwandler', font=('Helvetica', 12))
-label_wahrung.pack(pady=20)
+label_wahrung = tk.Label(root, text='Währungsumwandler', font=('Arial', 12)) #Währungsumwandler Text (Überschrift)
+label_wahrung.pack(pady=20) #Position
 
+frame = tk.Frame(root) #Frame von 'dropdown1' und 'dropdown2'
+frame.pack(pady=10) #Position
 
-frame = tk.Frame(root)
-frame.pack(pady=10)
+#Auswählbare Währungen
+options = ['EUR', 'USD', 'TRY', 'CNY', 'RUB', 'GBP', 'COP', 'HUF', 'PHP', 'PLN', 'KGS', 'JPY', 'THB', 'ALL', 'KRW', 'SOS', 'VB'] 
 
-options = ['EUR', 'TRY', 'CNY', 'USD', 'COP', 'HUF', 'PHP', 'VB', 'PLN', 'RUB', 'KGS', 'GBP', 'JPY', 'THB', 'ALL', 'KRW', 'SOS']
+#Auswählbare Währungen in den Dropdowns
+selected_option = tk.StringVar(root) #Auswahl bekommt einen Wert
+selected_option.set(options[0]) #Auswahl wird bei Start auf 0 (EUR) gesetzt
+dropdown1 = tk.OptionMenu(frame, selected_option, *options) #Erste Dropdown Auswahl
+dropdown1.pack(side=tk.LEFT) #Position 
+dropdown1.config(font=('Courier', 12)) #Schrift
 
+selected_option2 = tk.StringVar(root) #Auswahl bekommt einen Wert
+selected_option2.set(options[1]) #Auswahl wird bei Start auf 1 (TRY) gesetzt
+dropdown2 = tk.OptionMenu(frame, selected_option2, *options) #Zweite Dropdown Auswahl
+dropdown2.pack(side=tk.RIGHT) #Position 
+dropdown2.config(font=('Courier', 12)) #Schrift
 
-selected_option = tk.StringVar(root)
-selected_option.set(options[0])
-
-dropdown1 = tk.OptionMenu(frame, selected_option, *options)
-dropdown1.pack(side=tk.LEFT)
-
-selected_option2 = tk.StringVar(root)
-selected_option2.set(options[1])
-
-dropdown2 = tk.OptionMenu(frame, selected_option2, *options)
-dropdown2.pack(side=tk.RIGHT)
-
-
-
-
+###Funktion zum Berechnen/Umwandeln
 def berechnen():
-    ###Funktion zum Umwandeln
-    result = client.latest()
-    #Quellwaehrung:
-  
-    qw =(selected_option.get)()
-    q = result['data'][qw]['value'] 
-    #Zielwaehrung
+    result = client.latest() #Ergebnisse sollen die neusten Werte haben
+
+    #Quellwährung
+    qw = selected_option.get() #Auswahl aus Dropdown wird gegettet
+    if qw == 'VB':
+        q = 950 / 10 # 950 V-Bucks = 10 USD oder 1 V-Buck = 10 / 950 USD (Alte Preise)
+    else:
+        q = result['data'][qw]['value']  # result=client.latest, data=Wechselkurse, qw=Quellwährung, value= Wechselkurs der Währung
+        #Kurzgesagt: Wechselkurse der Quellwährung
+    #Zielwährung
     zw = selected_option2.get()
-    z = result['data'][zw]['value']
-    #umzuwandeln = 1
+    if zw == 'VB':
+        z = 950 / 10
+    else:
+        z = result['data'][zw]['value'] #result=client.latest, data=Wechselkurse, zw=Zielwährung, value = Wechselkurs der Währung
+        #Kurzgesagt: Wechselkurse der Zielwährung
 
-    ergebnis =(float (entry_var.get())/q) * z
-    output_var.set(round(ergebnis,2))
-   
+    ergebnis = (float(entry_var.get()) / q) * z #Rechnung
+    output_var.set(round(ergebnis, 2)) #Gerundet auf zwei Nachkommastellen
+
+###Funktion zum Zurücksetzen
 def reset():
-  entry_var.set('')
-  output_var.set('')
+    entry_var.set('')
+    output_var.set('')
 
-
-entry_var = tk.StringVar()
-entry = tk.Entry(root, textvariable=entry_var)
+###Ein- und Ausgabefelder
+entry_var = tk.StringVar() #Speichert die Eingabe
+entry = tk.Entry(root, textvariable=entry_var) #Eingabefeld
 entry.pack()
-output_var = tk.StringVar()
-output_field = tk.Entry(root, textvariable=output_var, state='readonly')
+output_var = tk.StringVar() #Speichert das Ergebnis
+output_field = tk.Entry(root, textvariable=output_var, state='readonly') #Ausgabefeld welches durch readonly schreibgeschützt ist
 output_field.pack()
 
-
-
-berechnen_button = tk.Button(root, text='Berechnen', command=berechnen)
-berechnen_button.pack(padx=150,pady=5)
-zurucksetzen_button = tk.Button(root, text='Zurücksetzen', command=reset)
-zurucksetzen_button.pack(padx=10)
+###Knöpfe für das Berechnen und Zurücksetzen
+berechnen_button = tk.Button(root, text='Berechnen', font=('Courier', 10), command=berechnen) 
+berechnen_button.pack(padx=150, pady=5) #Position
+zurucksetzen_button = tk.Button(root, text='Zurücksetzen', font=('Courier', 10), command=reset) 
+zurucksetzen_button.pack(padx=10) #Position
 
 # Herr Alkan bester Lehrer ❤
 
-
-'''
-# Legende/Wechselkurse
-def create_window():
-    window = tk.Toplevel(root)
-    
-b1 = tk.Button(root, text="Wechselkurse", command=create_window)
-b1.pack(side=tk.RIGHT, anchor=tk.SE, padx=10, pady=10)  # Move the button to the slightly up and left
-'''
-
+###Legende
 def open_legende():
-    
     rootlegende = tk.Tk()
-    rootlegende.title ('Legende')
+    rootlegende.title('Legende')
     rootlegende.geometry('300x350')
 
-    label_euro =tk.Label(rootlegende, text='EUR=EURO')
+    ###Texte 
+    label_euro = tk.Label(rootlegende, text='EUR=EURO') 
     label_euro.pack()
 
-    label_lira=tk.Label(rootlegende, text='TRY=TÜRKISCHE LIRA(TÜRKEI)')
-    label_lira.pack()
-
-    label_yuan=tk.Label(rootlegende, text='CNY=RENMINBI YUAN(CHINA)')
-    label_yuan.pack()
-
-    label_dollar=tk.Label(rootlegende, text='USD=US-DOLLAR(USA)')
+    label_dollar = tk.Label(rootlegende, text='USD=US-DOLLAR(USA)')
     label_dollar.pack()
 
-    label_peso=tk.Label(rootlegende, text='COP=PESO(KOLUMBIEN)')
-    label_peso.pack()
+    label_lira = tk.Label(rootlegende, text='TRY=TÜRKISCHE LIRA(TÜRKEI)')
+    label_lira.pack()
 
-    label_forint=tk.Label(rootlegende, text='HUF=FORINT(UNGARN)')
-    label_forint.pack()
+    label_yuan = tk.Label(rootlegende, text='CNY=RENMINBI YUAN(CHINA)')
+    label_yuan.pack()
 
-    label_philipinische_peso=tk.Label(rootlegende, text='PHP= PESO(PHILIPPINIEN)')
-    label_philipinische_peso.pack()
-
-    label_vbucks=tk.Label(rootlegende, text='VB=V-BUCKS(FORTNITE)')
-    label_vbucks.pack()
-
-    label_zloty=tk.Label(rootlegende, text='PLN=ZLOTY(POLEN)')
-    label_zloty.pack()
-
-    label_rubel=tk.Label(rootlegende, text='RUB=RUBEL(RUSSLAND)')
+    label_rubel = tk.Label(rootlegende, text='RUB=RUBEL(RUSSLAND)')
     label_rubel.pack()
 
-    label_pfund=tk.Label(rootlegende, text='GBP=PFUND STERLING(VEREINIGTES KÖNIGREICH)')
+    
+    label_pfund = tk.Label(rootlegende, text='GBP=PFUND STERLING(VEREINIGTES KÖNIGREICH)')
     label_pfund.pack()
+    
+    label_peso = tk.Label(rootlegende, text='COP=PESO(KOLUMBIEN)')
+    label_peso.pack()
 
-    label_yen=tk.Label(rootlegende, text='JPY=YEN(JAPAN)')
+    label_forint = tk.Label(rootlegende, text='HUF=FORINT(UNGARN)')
+    label_forint.pack()
+
+    label_philipinische_peso = tk.Label(rootlegende, text='PHP=PESO(PHILIPPINIEN)')
+    label_philipinische_peso.pack()
+
+    label_zloty = tk.Label(rootlegende, text='PLN=ZLOTY(POLEN)')
+    label_zloty.pack()
+
+
+    label_yen = tk.Label(rootlegende, text='JPY=YEN(JAPAN)')
     label_yen.pack()
 
-    label_baht=tk.Label(rootlegende, text='THB=BAHT(THAILAND)')
+    label_baht = tk.Label(rootlegende, text='THB=BAHT(THAILAND)')
     label_baht.pack()
 
-    label_lek=tk.Label(rootlegende, text='ALL=LEK(ALBANIEN)')
+    label_lek = tk.Label(rootlegende, text='ALL=LEK(ALBANIEN)')
     label_lek.pack()
 
-    label_won=tk.Label(rootlegende, text='KRW=WON(SÜDKOREA)')
+    label_won = tk.Label(rootlegende, text='KRW=WON(SÜDKOREA)')
     label_won.pack()
 
-    label_sol=tk.Label(rootlegende, text='SOL=Somalia Schilling (Somalia)')
+    label_sol = tk.Label(rootlegende, text='SOL=Somalia Schilling (Somalia)')
     label_sol.pack()
 
-b2 = tk.Button(root, text="Legende", command=open_legende)
-b2.pack(side=tk.RIGHT, anchor=tk.SE, padx=10, pady=10) 
-# New button for "Legende"
+    label_vb = tk.Label(rootlegende, text='VB=VBucks (Fortnite)')
+    label_vb.pack()
 
-
-
-
-
-
-
+b2 = tk.Button(root, text="Legende", font=('Courier', 10), command=open_legende) #Knopf 'Legende'
+b2.pack(side=tk.RIGHT, anchor=tk.SE, padx=10, pady=10) #Position
 
 root.mainloop()
-
-    
-
-
-
-
-
-
-
-
